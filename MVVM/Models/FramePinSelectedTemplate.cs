4ebf618957a6aa99faf9ce54e97d08c83b134826
ruben <ruben.calducho@sofka.com.co>
2024-03-13 13:55:47 -0500

    Rdc - Desarrollo base

---
+++ MVVM/Models/FramePinSelectedTemplate.cs
@@ -0,0 +1,9 @@
+﻿namespace RutaSeguimientoApp.MVVM.Models
+{
+	public class FramePinSelectedTemplate
+	{
+        public int MyProperty { get; set; }
+        public string Tittle { get; set; }
+
+    }
+}
